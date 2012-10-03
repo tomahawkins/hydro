d617706ac3944f048041de780389c07504a1da12
Tom Hawkins <tomahawkins@gmail.com>
2012-10-02 22:00:32 -0500

    Layout to svg.

--- Hull.hs
+++ Hull.hs
@@ -3,15 +3,29 @@ module Main (main) where
 import Language.DevSurf
 
 main :: IO ()
-main = writeFile "Hull.stl" $ exportSTL sponson
+main = do
+  print $ canFlatten 0.1 twist
+  --print $ map (canFlatten 0.5) sponson
+  writeFile "Hull.stl" $ exportSTL $ concatMap (meshPanel LHR) sponson
+  writeFile "Hull.svg" $ exportSVG $ layout sponson
 
-sponson :: Mesh
-sponson = concat $ map (meshPanel LHR)
-  [ loft rail chine
-  , loft chine bottom
-  , loft bottom top
-  , loft top rail
-  , [last bottom, last top, last chine, last rail]
+layout :: [Panel] -> [Curve]
+layout panels = move (20, 20, 0) $ scale (660, 660, 1) $ f $ map (head . bound . (: []) . profile . flatten) panels
+  where
+  f :: [Curve] -> [Curve]
+  f [] = []
+  f (a : b) = a : move (0, my, 0) (f b)
+    where
+    my' = maximum [ y | (_, y, _) <- a ]
+    my = my' * 1.15
+
+sponson :: [Panel]
+sponson =
+  [ sidePanel
+  , bottomPanel
+  , insidePanel
+  , topPanel
+  , backPanel
   ]
   where
   rail = subdivideN 5
@@ -24,21 +38,24 @@ sponson = concat $ map (meshPanel LHR)
   bottom = rotateY ( 90 * pi / 180) $ scale (0.4 , 1, 1) rail
   top    = rotateY (-90 * pi / 180) $ scale (0.15, 1, 1) rail
 
-{-
-  bound :: [Curve] -> [Curve]
-  bound curves = [ move m curve | curve <- curves ]
-    where
-    (xs, ys, _) = unzip3 $ concat curves
-    m = (- minimum xs, - minimum ys, 0)
--}
+  sidePanel   = loft rail chine
+  bottomPanel = loft chine bottom
+  insidePanel = loft bottom top
+  topPanel    = loft top rail
+  backPanel   = [last bottom, last top, last chine, last rail]
 
-{-
-twist :: Mesh
-twist = meshPanel LHR $ loft c1 c2
+
+bound :: [Curve] -> [Curve]
+bound curves = [ move m curve | curve <- curves ]
+  where
+  (xs, ys, _) = unzip3 $ concat curves
+  m = (- minimum xs, - minimum ys, 0)
+
+twist :: Panel
+twist = loft c1 c2
   where
   c1 = subdivideN 3 [(0, 0, 0), (1, 1, 0)]
   c2 = subdivideN 3 [(1, 0, 1), (0, 1, 1)]
--}
 
 -- As a test for developable, check that the dot product between corresponding normal vectors is below a certain threshold.
 
