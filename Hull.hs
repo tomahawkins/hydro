5c1f219bb9124931b12f22232340112c9c99b0f0
Tom Hawkins <tomahawkins@gmail.com>
2014-03-27 15:20:40 -0500

    Updates.

--- Hull.hs
+++ Hull.hs
@@ -4,8 +4,8 @@ import Language.DevSurf
 
 main :: IO ()
 main = do
-  print $ canFlatten 0.1 twist
-  --print $ map (canFlatten 0.5) sponson
+  --print $ canFlatten 0.1 twist
+  print $ map (canFlatten 0.5) sponson
   writeFile "Hull.stl" $ exportSTL $ concatMap (meshPanel LHR) sponson
   writeFile "Hull.svg" $ exportSVG $ layout sponson
 
@@ -28,21 +28,33 @@ sponson =
   , backPanel
   ]
   where
+  {-
   rail = subdivideN 5
-    [ (0  , 1   , 0)
-    , (0.2, 0.85, 0)
-    , (0.3, 0.5 , 0)
-    , (0.3, 0   , 0)
+    [ (0.2, 1   , 0)
+    , (0.3, 0.85, 0)
+    , (0.4, 0.5 , 0)
+    , (0.4, 0   , 0)
     ]
   chine  = rotateY ( 30 * pi / 180) $ scale (0.8 , 1, 1) rail
   bottom = rotateY ( 90 * pi / 180) $ scale (0.4 , 1, 1) rail
   top    = rotateY (-90 * pi / 180) $ scale (0.15, 1, 1) rail
+  -}
+  fip  = (1.0, 0.0,  0.0)
+  fop  = (0.9, 0.2,  0.0)
+  ritp = (0.0, 0.0,  0.0)
+  ribp = (0.0, 0.0, -0.3)
+  rotp = (0.0, 0.4,  0.0)
+  robp = (0.0, 0.3, -0.2)
+  itl  = [ritp, fip]
+  ibl  = [ribp, fip]
+  otl  = [rotp, fop]
+  obl  = [robp, fop]
 
-  sidePanel   = loft rail chine
-  bottomPanel = loft chine bottom
-  insidePanel = loft bottom top
-  topPanel    = loft top rail
-  backPanel   = [last bottom, last top, last chine, last rail]
+  sidePanel   = loft otl obl
+  bottomPanel = loft obl ibl
+  insidePanel = loft ibl itl
+  topPanel    = loft itl otl
+  backPanel   = loft [ritp, rotp] [ribp, robp]
 
 
 bound :: [Curve] -> [Curve]
